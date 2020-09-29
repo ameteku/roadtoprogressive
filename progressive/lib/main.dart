5ad9c3de77a970d1cd1470299747fcef6e87b0a6
michael ameteku <31578930+ameteku@users.noreply.github.com>
2020-09-29 08:48:56 -0400

    First commit
    Created the appbr and added images

---
+++ progressive/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp());
+}
+
+class progressiveinsurance extends StatefulWidget {
+  @override
+  _progressiveinsuranceState createState() => _progressiveinsuranceState();
+}
+
+class _progressiveinsuranceState extends State<progressiveinsurance> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Progressive"),
+      ),
+      body: Column(
+        children: <widget>[],
+      ),
+    );
+  }
+}
